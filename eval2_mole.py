229faeb0fb39fdb4b4dd11dbd3946600baad09c1
MIN <mhroh01@naver.com>
2025-12-07 01:14:37 +0900

    debug

--- eval2_mole.py
+++ eval2_mole.py
@@ -216,8 +216,6 @@ def generate_with_model(prompt_list, tokenizer, model, device="cuda", max_new_to
         print(model_only_text)
         print("="*80 + "\n")
         
-        break
-        
     return full_outputs,  model_only_outputs
 
 # =================== SOTA COMPARISON ====================
